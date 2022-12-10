60498345668f3b6ccce942e924348fc826f72709
harshjain883 <115611849+harshjain883@users.noreply.github.com>
2022-12-10 23:57:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ get_queue = {}
 
 API_ID = int(getenv("API_ID", "13412363")) 
 API_HASH = getenv("API_HASH", "2363839f7780d48f27c9c01bb1a0d01b")
-BOT_TOKEN = getenv("BOT_TOKEN", "5385446612:AAHaBL6-WhnUTPmfLKDscwWAgJtSOTnbgIk")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "150"))
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5156537452").split()))
 SUPPORT = getenv("SUPPORT", "venombot_support")
